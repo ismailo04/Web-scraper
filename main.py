4c246b9b2d5a2c057b98053591aabb049f81a18d
ismailo04 <89299825+ismailo04@users.noreply.github.com>
2024-01-17 15:52:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
-
 response = requests.get(URL)
 website_html = response.text
 
@@ -18,16 +16,3 @@ movies = movie_titles[::-1]
 with open("movies.txt", mode="w") as file:
     for movie in movies:
         file.write(f"{movie}\n")
-
-
-'''
-FAQ: Empire's website has changed!
-
-When this lesson was created, I used this URL for the project: 
-URL = "https://www.empireonline.com/movies/features/best-movies-2/"
-
-However, Empire has since changed their website. You can see this when you inspect the movie title elements. 
-You'll see that the h3 with the class "title" is no longer there. 
-To use exactly the same code as per the solution, we can use a cached version of the website from the Internet Archive's Wayback Machine.
-
-'''
